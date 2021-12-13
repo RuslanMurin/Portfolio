087c298bf7624afa88b01fd222d798de26ae61e7
Ruslan <87794726+RMurin@users.noreply.github.com>
2021-12-13 14:29:25 +0300

    UPD

--- iTunesAPI/iTunesAPI/Cells/ResultCollectionViewCell.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class ResultCollectionViewCell: UICollectionViewCell {
-    @IBOutlet weak var albumImage: UIImageView!
-    @IBOutlet weak var albumLabel: UILabel!
-    @IBOutlet weak var artistLabel: UILabel!
-}
--- iTunesAPI/iTunesAPI/Cells/SongTableViewCell.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-class SongTableViewCell: UITableViewCell {
-    @IBOutlet weak var numberLabel: UILabel!
-    @IBOutlet weak var songLabel: UILabel!
-    @IBOutlet weak var timeLabel: UILabel!
-}
